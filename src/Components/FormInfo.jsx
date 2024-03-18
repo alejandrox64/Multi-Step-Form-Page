cce91e3c9fddf40742a40533d4fc192f76dfe0c3
Alejandro Rojas <ale402152@gmail.com>
2024-03-18 11:35:40 -0300

    finalized formInfo component styles and structure

--- src/Components/FormInfo.jsx
+++ src/Components/FormInfo.jsx
@@ -3,21 +3,22 @@ import styles from "../styles/FormInfo.module.scss";
 
 const FormInfo = () => {
   return (
-    <form className={styles.formInfo}>
-      <label>
-        Name
-        <input type="text" />
-      </label>
-      <label>
-        Email Adress
-        <input type="email" />
-      </label>
-      <label>
-        Phone Number
-        <input type="number" />
-      </label>
-      <input type="submit">Enviar</input>
-    </form>
+    <div className={styles.formContainer}>
+      <form className={styles.formInfo}>
+        <label>
+          <span>Name</span>
+          <input type="text" required />
+        </label>
+        <label>
+          <span>Email Adress</span>
+          <input type="email" required />
+        </label>
+        <label>
+          <span>Phone Number</span>
+          <input type="number" required />
+        </label>
+      </form>
+    </div>
   );
 };
 
--- src/MainCardForm.jsx
+++ src/MainCardForm.jsx
@@ -1,4 +1,4 @@
-import AddOns from "./Components/AddOns";
+import FormInfo from "./Components/FormInfo";
 import Steps from "./Components/Steps";
 import styles from "./styles/MainCardForm.module.scss";
 
@@ -9,7 +9,8 @@ function MainCardForm() {
       <div className={styles.mainContent}>
         <h1>Personal info</h1>
         <p>Please provide your name, email address, and phone number.</p>
-        <AddOns />
+        <FormInfo />
+        <button className={styles.nextStepButton}>Next Step</button>
       </div>
     </main>
   );
